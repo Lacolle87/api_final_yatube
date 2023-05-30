3e04e064ccd4c9b53691fab4ff1c11869878615d
Nikita Kuznetsov <123502774+Lacolle87@users.noreply.github.com>
2023-05-30 16:59:26 +0300

    review 2 fix

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,7 +5,13 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-        )
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+    # я сделал так, но не могу пройти тест яндекса при загрузке
+    # def has_object_permission(self, request, view, obj):
+    #     return (
+    #             request.method in permissions.SAFE_METHODS
+    #             or obj.author == request.user
+    #     )
