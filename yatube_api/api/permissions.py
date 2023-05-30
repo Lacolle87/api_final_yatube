9ef3a05d4718b8a4cd41968605eff2b5b5cc1e99
Nikita Kuznetsov <123502774+Lacolle87@users.noreply.github.com>
2023-05-30 12:09:12 +0300

    review 2 fix

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,5 +5,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user)
+        return (
+                request.method in permissions.SAFE_METHODS or
+                obj.author == request.user
+        )
